f882e00bbad4bc9c4f03104e8604510efca9cbe3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 21:36:34 -0300

    fim

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("escreva qualquer merda ai:");
+string x = Console.ReadLine()!
+.Replace ("a", "4")
+.Replace ("a", "4")
+.Replace ("e", "3")
+.Replace ("i", "1")
+.Replace ("o", "0")
+.Replace ("t", "7")
+.Replace ("s", "5")
+;
+
+Console.WriteLine($"{x}".ToLower());
